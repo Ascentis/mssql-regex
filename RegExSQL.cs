ee82c1c516723328dd7812fdf60a0bd460fc0dc4
Sebastian Battig <sbattig@ascentis.com>
2020-06-09 00:27:45 -0300

    Added a couple of more functions

--- RegExSQL.cs
+++ RegExSQL.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Data.SqlTypes;
 using System.Linq;
 using Microsoft.SqlServer.Server;
@@ -47,9 +48,35 @@ public class RegExCompiled
         return Regex.Match(input, pattern, RegexOptions.Compiled).Value;
     }
 
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static string RegExCompiledMatchIndexed(string input, string pattern, int index)
+    {
+        var matches = Regex.Matches(input, pattern, RegexOptions.Compiled);
+        return index >= matches.Count ? "" : matches[index].Value;
+    }
+
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static string RegExCompiledMatchGroup(string input, string pattern, int group)
+    {
+        return Regex.Match(input, pattern, RegexOptions.Compiled).Groups[group].Value;
+    }
+
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static string RegExCompiledMatchGroupIndexed(string input, string pattern, int group, int index)
+    {
+        var matches = Regex.Matches(input, pattern, RegexOptions.Compiled);
+        return index >= matches.Count ? "" : matches[index].Groups[group].Value;
+    }
+
     [SqlFunction(IsDeterministic = true, IsPrecise = true, FillRowMethodName="FillRow", TableDefinition="STR NVARCHAR(MAX)")]
     public static IEnumerable RegExCompiledMatches(string input, string pattern)
     {
         return Regex.Matches(input, pattern).Cast<Match>().Select(m => m.Value).ToArray();
+    }
+
+    [SqlFunction(IsDeterministic = true, IsPrecise = true, FillRowMethodName = "FillRow", TableDefinition = "STR NVARCHAR(MAX)")]
+    public static IEnumerable RegExCompiledMatchesGroup(string input, string pattern, int group)
+    {
+        return Regex.Matches(input, pattern).Cast<Match>().Select(m => m.Groups[group].Value).ToArray();
     }
 };
